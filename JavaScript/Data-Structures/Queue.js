f401741e94cf6b5dc2750e6f8c8fd155c28c0156
robertKegel <kegel.robert@gmail.com>
2020-08-14 18:16:28 -0400

    Create Queue.js

---
+++ JavaScript/Data-Structures/Queue.js
@@ -0,0 +1,25 @@
+const List = require('./LinkedList');
+
+class Queue {
+    constructor(limit=1000){
+        this._list = new List();
+        this._limit = limit;
+    }
+    
+    getLength() {
+        return this._list.getLength();
+    }
+    
+    push(data){
+        this._list.push(data);
+    }
+    
+    pop(){
+        return this._list.pop();
+    }
+    
+    getArray(){
+        return this._list.returnListArray();
+    }
+    
+}
